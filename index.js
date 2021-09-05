adc57d8dee6df547946580ee4a1f44219958fc6b
LeomharT <leo.liao.lzy@gmail.com>
2021-09-05 22:07:20 +0800

    重命名文件夹,main函数

--- index.js
+++ index.js
@@ -20,30 +20,32 @@ export const AliDNS = {
     database: "react_house",
     port: 3306
 };
-
-
 const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
-app.get("/img/*", (req, res) =>
+void function main()
 {
-    res.sendFile(path.resolve() + req.url);
-});
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
 
-const editUserInfo = new EditUserInfo(app);
-editUserInfo.GetProvince();
-editUserInfo.InitCity();
-editUserInfo.GetHouseParams();
+    app.get("/img/*", (req, res) =>
+    {
+        res.sendFile(path.resolve() + req.url);
+    });
 
-const houseLists = new HouseLists(app);
-houseLists.GetHouseExhibitList();
-houseLists.GetHouseDetailInfo();
-houseLists.GetHouseVrSceneArray();
-houseLists.GetHouseVrSceneInfo();
+    const editUserInfo = new EditUserInfo(app);
+    editUserInfo.GetProvince();
+    editUserInfo.InitCity();
+    editUserInfo.GetHouseParams();
 
-app.listen(3065, () =>
-{
-    console.log("server runing at localhost:3065");
-});
+    const houseLists = new HouseLists(app);
+    houseLists.GetHouseExhibitList();
+    houseLists.GetHouseDetailInfo();
+    houseLists.GetHouseVrSceneArray();
+    houseLists.GetHouseVrSceneInfo();
+
+    app.listen(3065, () =>
+    {
+        console.log("server runing at localhost:3065");
+    });
+}();
