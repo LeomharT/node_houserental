f57834e8676ee64670ba2d70e9e294f469e2ac17
LeomharT <leo.liao.lzy@gmail.com>
2021-10-13 14:13:06 +0800

    沙箱环境下的收银台只能这样

--- src/AliPay/AliPay.cjs
+++ src/AliPay/AliPay.cjs
@@ -21,18 +21,22 @@ module.exports = class AliPay
     {
         this.app.post('/PayOrder', async (req, res) =>
         {
+            //不解析FormData如果是JSON数据直接在body里🐂
+            console.log(req.body);
             const formData = new AlipayFormData();
             formData.setMethod("get");
+            //支付成功主动推送到指定的界面
             formData.addField('notifyUrl', 'http://localhost:3000/HouseList/ConfirmOrder');
+            //支付完成后返回的界面
+            formData.addField("resultUrl", 'http://localhost:3000/Home');
             formData.addField('bizContent', {
-                outTradeNo: '15693801273221', // 商户订单号,64个字符以内、可包含字母、数字、下划线,且不能重复
+                outTradeNo: '15691322732221', // 商户订单号,64个字符以内、可包含字母、数字、下划线,且不能重复,订单如果相同则继续去支付
                 productCode: 'FAST_INSTANT_TRADE_PAY', // 销售产品码，与支付宝签约的产品码名称,仅支持FAST_INSTANT_TRADE_PAY
-                totalAmount: '0.01', // 订单总金额，单位为元，精确到小数点后两位
+                totalAmount: '99', // 订单总金额，单位为元，精确到小数点后两位
                 subject: '商品', // 订单标题
                 body: '商品详情', // 订单描述
+                subject: "租房噢", //商品标题
             });
-            formData.addField("resultUrl", 'http://localhost:3000/Home');
-            console.log(formData);
             const aliPayUrl = await this.aliPaySdk.exec('alipay.trade.page.pay', {}, { formData });
             res.send(JSON.stringify(aliPayUrl));
         });
