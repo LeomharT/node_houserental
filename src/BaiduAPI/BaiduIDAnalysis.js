fdce19b1948b86a4f8483db68bd5b5099cf053a6
LeomharT <leo.liao.lzy@gmail.com>
2021-12-17 10:09:34 +0800

    没什么用的上传方法用于不让前端报错

--- src/BaiduAPI/BaiduIDAnalysis.js
+++ src/BaiduAPI/BaiduIDAnalysis.js
@@ -1,6 +1,6 @@
 import request from "request";
-
-
+import multiparty from 'multiparty';
+import fs from 'fs';
 const config = {
     grant_type: "client_credentials",
     client_id: "YT9372i1uwblqLOVKbKkKHZ5",
@@ -23,6 +23,23 @@ export default class BaiduIDAnalysis
                     res.send(body);
                 }
             );
+        });
+    };
+    FormatImageToBase64 = () =>
+    {
+        this.app.post("/FormatImageToBase64", (req, res) =>
+        {
+            const formData = new multiparty.Form();
+            //fields是字段,文件在file里面
+            formData.parse(req, (err, fields, file) =>
+            {
+                if (err) throw new Error;
+                const uploadImg = file.file[0];
+                fs.readFile(uploadImg.path, (err, data) =>
+                {
+                    if (err) throw new Error;
+                });
+            });
         });
     };
 }
